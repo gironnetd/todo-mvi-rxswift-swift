a9e8816af0c1e6d29755c410ce4411efe6ba38b7
Damien Gironnet <gironnetd@yahoo.fr>
2021-12-01 16:46:11 +0100

    Initial Commit

---
+++ todo-mvi-rxswift-swiftTests/todo_mvi_rxswift_swiftTests.swift
@@ -0,0 +1,33 @@
+//
+//  todo_mvi_rxswift_swiftTests.swift
+//  todo-mvi-rxswift-swiftTests
+//
+//  Created by damien on 01/12/2021.
+//
+
+import XCTest
+@testable import todo_mvi_rxswift_swift
+
+class todo_mvi_rxswift_swiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
